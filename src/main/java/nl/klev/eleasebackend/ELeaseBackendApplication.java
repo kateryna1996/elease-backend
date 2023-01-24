c86093f99fe48afeb999eb7aa9cf0d8743db8901
Kateryna Levchenko <k.levchenko28@gmail.com>
2023-01-24 10:45:32 +0100

    first commit

---
+++ src/main/java/nl/klev/eleasebackend/ELeaseBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.klev.eleasebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELeaseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELeaseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/klev/eleasebackend/ELeaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.klev.eleasebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELeaseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
